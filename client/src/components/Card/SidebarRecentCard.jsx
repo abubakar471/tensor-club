e8cbb87e403e5b261fb17bf8a8599005f03831f9
abubakar471 <abdurjoy2001@gmail.com>
2023-10-08 22:29:16 +0600

    sidebar recent activity container added

--- client/src/components/Card/SidebarRecentCard.jsx
+++ client/src/components/Card/SidebarRecentCard.jsx
@@ -6,7 +6,7 @@ const SidebarRecentCard = () => {
   return (
     <div className="w-full flex items-center gap-x-2 my-4 cursor-pointer
      hover:bg-gradient-to-r hover:from-blue-900 
-     transition-all duration-300 ease-in-out p-2">
+     transition-all duration-150 ease-in-out p-2 hover:translate-x-2 ">
       <ChatBubbleOutlineTwoTone className="text-white" />
       <h1 className="text-gray-300">Metaphor project</h1>
     </div>
